5735031f22c2211fd00175e29c9a745a6c59395f
Rodrigo Alejandro <rodrigo-b@live.com.mx>
2023-05-12 17:18:16 -0600

    Agregada la funcion de checar a donde mandamos segun el rol que tenga el usuario

--- app/src/main/java/com/example/cafe2/AuthActivity.kt
+++ app/src/main/java/com/example/cafe2/AuthActivity.kt
@@ -3,18 +3,24 @@ package com.example.cafe2
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
 import androidx.appcompat.app.AlertDialog
 import com.example.cafe2.AuthActivity
 import com.example.cafe2.R.layout.activity_auth
 import com.google.firebase.analytics.FirebaseAnalytics
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.FirebaseFirestore
 import kotlinx.android.synthetic.main.activity_auth.emailEditText
 import kotlinx.android.synthetic.main.activity_auth.loginButton
 import kotlinx.android.synthetic.main.activity_auth.passwordEditText
 import kotlinx.android.synthetic.main.activity_auth.signUpButton
+import kotlinx.android.synthetic.main.activity_nuevo_producto.editTextText
 
 class AuthActivity : AppCompatActivity() {
+
+    private val db = FirebaseFirestore.getInstance()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_auth)
@@ -28,24 +34,41 @@ class AuthActivity : AppCompatActivity() {
         title = "Autentificacion"
 
        signUpButton.setOnClickListener{
+           print("Boton picadoooooo")
            showRegistrar()
        }
 
         loginButton.setOnClickListener{
+            Log.i("Boton de login clickeadoooo","Mensaje de prueba")
             if ( emailEditText.text.isNotEmpty() && passwordEditText.text.isNotEmpty()){
+                //Aqui autentificamos que este el usuario dado de alta en la base de datos
                 FirebaseAuth.getInstance()
                     .signInWithEmailAndPassword(emailEditText.text.toString(),passwordEditText.text.toString())
-                    .addOnCompleteListener{ if (it.isSuccessful){
-                        showHome(it.result?.user?.email?: "", ProviderType.BASIC)
+                    .addOnCompleteListener{
+                        if (it.isSuccessful){
+                            //aqui accedemos a la base de datos para checar el rol
+                            val Rol = db.collection("Usuarios").document(emailEditText.text.toString()).get().addOnSuccessListener {
+                                val Rol = (it.get("Rol") as String?).toString()
+                                //Mandamos a la pantalla principal de cliente
+                                if(Rol.equals("Cliente")){
+                                    showHome(emailEditText.toString())
+                                }
+                                //Mandamos a la pantalla principal de Administrador
+                                if(Rol.equals("Administrador")){
+                                    showRegistrar()
+                                }
+                                //Mandamos a la pantalla principal de Cajero
+                                if(Rol.equals("Cajero")){}
+                            }
                     }
                     else { showAlert() }
                     }
             }
         }
-
-
-
     }
+
+
+
 
     private fun showAlert(){
         val builder = AlertDialog.Builder(this)
@@ -57,10 +80,9 @@ class AuthActivity : AppCompatActivity() {
 
     }
 
-    private fun showHome(email:String, provider: ProviderType){
+    private fun showHome(email:String){
         val homeIntent = Intent(this, HomeActivity::class.java).apply {
             putExtra("email",email)
-            putExtra("provider",provider.name)
         }
         startActivity(homeIntent)
 
