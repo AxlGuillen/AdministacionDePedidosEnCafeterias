d1d83b483bf7267646a75986b7659b5f8b7154e4
chavezlopezabigail28 <chavezlopezabigail28@gmail.com>
2023-05-12 18:45:47 -0600

    Pantalla de Pregunta y MenuUsuarios

--- app/src/main/java/com/example/cafe2/OperacionExitosa.kt
+++ app/src/main/java/com/example/cafe2/OperacionExitosa.kt
@@ -3,14 +3,14 @@ package com.example.cafe2
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import kotlinx.android.synthetic.main.activity_operacion_exitosa.btnOk
+import kotlinx.android.synthetic.main.activity_operacion_exitosa.btnSi
 
 class OperacionExitosa : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_operacion_exitosa)
 
-        btnOk.setOnClickListener {
+        btnSi.setOnClickListener {
             val homeIntent = Intent(this, NuevoProducto::class.java).apply {
             }
             startActivity(homeIntent)
