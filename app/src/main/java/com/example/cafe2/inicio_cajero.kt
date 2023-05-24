b31518b94c9ca117fc485ea69eb1c76e5ea3fdd9
Guillermo Mendez <l20121017@morelia.tecnm.mx>
2023-05-23 18:22:34 -0600

    Creado Layout del Inicio del Cajero

---
+++ app/src/main/java/com/example/cafe2/inicio_cajero.kt
@@ -0,0 +1,11 @@
+package com.example.cafe2
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class inicio_cajero : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_inicio_cajero)
+    }
+}
