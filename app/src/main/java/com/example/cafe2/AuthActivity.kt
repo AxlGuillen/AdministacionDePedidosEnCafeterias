37a9b907cf703bfac0fe43e6c2d2b1f803a0178d
Guillermo Mendez <l20121017@morelia.tecnm.mx>
2023-05-27 20:38:59 -0600

    Modificacion de Cosas visuales y Validacion de que en campos de email hay texto para que el sistema no pete en caso de no haberlo

--- app/src/main/java/com/example/cafe2/AuthActivity.kt
+++ app/src/main/java/com/example/cafe2/AuthActivity.kt
@@ -30,104 +30,125 @@ class AuthActivity : AppCompatActivity() {
 
         firebaseAuth = Firebase.auth
 
-        val correo: TextView = findViewById (R.id.emailLogin)
+        var correo: TextView = findViewById(R.id.emailLogin)
 
         btnRestablecerContraseña.setOnClickListener {
             sendPasswordReset(correo.text.toString())
         }
 
-    //setup
+        //setup
         setup()
 
     }
-    private fun setup(){
+
+    private fun setup() {
         title = "Autentificacion"
 
-       signUpButton.setOnClickListener{
-           showRegistrar()
-       }
+        signUpButton.setOnClickListener {
+            showRegistrar()
+        }
 
-        loginButton.setOnClickListener{
-            Log.i("Boton de login clickeadoooo","Mensaje de prueba")
-            if ( emailLogin.text.isNotEmpty() && passwordEditText.text.isNotEmpty()){
+        loginButton.setOnClickListener {
+            Log.i("Boton de login clickeadoooo", "Mensaje de prueba")
+            if (emailLogin.text.isNotEmpty() && passwordEditText.text.isNotEmpty()) {
                 //Aqui autentificamos que este el usuario dado de alta en la base de datos
                 FirebaseAuth.getInstance()
-                    .signInWithEmailAndPassword(emailLogin.text.toString(),passwordEditText.text.toString())
-                    .addOnCompleteListener{
-                        if (it.isSuccessful){
+                    .signInWithEmailAndPassword(
+                        emailLogin.text.toString(),
+                        passwordEditText.text.toString()
+                    )
+                    .addOnCompleteListener {
+                        if (it.isSuccessful) {
                             //aqui accedemos a la base de datos para checar el rol
-                            val Rol = db.collection("Usuarios").document(emailLogin.text.toString()).get().addOnSuccessListener {
-                                val Rol = (it.get("Rol") as String?).toString()
-                                val Estado = (it.get("Estado") as Boolean?)
-                                //Mandamos a la pantalla principal de cliente
+                            val Rol =
+                                db.collection("Usuarios").document(emailLogin.text.toString()).get()
+                                    .addOnSuccessListener {
+                                        val Rol = (it.get("Rol") as String?).toString()
+                                        val Estado = (it.get("Estado") as Boolean?)
+                                        //Mandamos a la pantalla principal de cliente
 
-                                if(Rol.equals("Cliente") && Estado==true){
-                                    //showHome(emailLogin.text.toString())
-                                    val homeIntent = Intent(this, inicio_clientes::class.java).apply {
-                                        putExtra("email",emailLogin.text.toString())
+                                        if (Rol.equals("Cliente") && Estado == true) {
+                                            //showHome(emailLogin.text.toString())
+                                            val homeIntent =
+                                                Intent(this, inicio_clientes::class.java).apply {
+                                                    putExtra("email", emailLogin.text.toString())
+                                                }
+                                            startActivity(homeIntent)
+                                            finish()
+                                        }
+                                        //Mandamos a la pantalla principal de Administrador
+                                        if (Rol.equals("Administrador")) {
+                                            //editar
+                                            val homeIntent =
+                                                Intent(this, inicio_Admin::class.java).apply {
+                                                    putExtra("email", emailLogin.text.toString())
+                                                }
+                                            startActivity(homeIntent)
+                                        }
+                                        //Mandamos a la pantalla principal de Cajero
+                                        if (Rol.equals("Cajero")) {
+                                            val homeIntent =
+                                                Intent(this, inicio_Admin::class.java).apply {
+                                                    putExtra("email", emailLogin.text.toString())
+                                                }
+                                            startActivity(homeIntent)
+                                        }
                                     }
-                                    startActivity(homeIntent)
-                                    finish()
-                                }
-                                //Mandamos a la pantalla principal de Administrador
-                                if(Rol.equals("Administrador")){
-                                    //editar
-                                    val homeIntent = Intent(this, inicio_Admin::class.java).apply {
-                                        putExtra("email",emailLogin.text.toString())
-                                    }
-                                    startActivity(homeIntent)
-                                }
-                                //Mandamos a la pantalla principal de Cajero
-                                if(Rol.equals("Cajero")){
-                                    val homeIntent = Intent(this, inicio_Admin::class.java).apply {
-                                        putExtra("email",emailLogin.text.toString())
-                                    }
-                                    startActivity(homeIntent)
-                                }
-                            }
-                    }
-                    else { showAlert() }
+                        } else {
+                            showAlert()
+                        }
                     }
             }
         }
     }
 
 
-
-
-    private fun showAlert(){
+    private fun showAlert() {
         val builder = AlertDialog.Builder(this)
         builder.setTitle("Error")
         builder.setMessage("Se ha producido un error autenticando al usuario")
-        builder.setPositiveButton("Aceptar",null)
+        builder.setPositiveButton("Aceptar", null)
         val dialog: AlertDialog = builder.create()
         dialog.show()
 
     }
 
 
-    private fun showHome(email:String){
+    private fun showHome(email: String) {
 
         val homeIntent = Intent(this, inicio_Admin::class.java).apply {
-            putExtra("email",email)
+            putExtra("email", email)
         }
         startActivity(homeIntent)
 
     }
 
-    private fun showRegistrar(){
-        val RegistrarIntent = Intent (this, RegistrarActivity::class.java)
+    private fun showRegistrar() {
+        val RegistrarIntent = Intent(this, RegistrarActivity::class.java)
         startActivity(RegistrarIntent)
     }
 
-    private fun sendPasswordReset (email: String){
-        firebaseAuth.sendPasswordResetEmail(email).addOnCompleteListener {task ->
-            if (task.isSuccessful){
-                Toast.makeText(baseContext,"Correo de recuperacion enviado.",Toast.LENGTH_LONG).show()
+    private fun sendPasswordReset(email: String) {
+        if (email.toString().isNotEmpty()) {
+            firebaseAuth.sendPasswordResetEmail(email).addOnCompleteListener { task ->
+
+                if (task.isSuccessful) {
+                    Toast.makeText(
+                        baseContext,
+                        "Correo de recuperacion enviado.",
+                        Toast.LENGTH_LONG
+                    ).show()
+                } else {
+                    Toast.makeText(baseContext, "Error al enviar el email.", Toast.LENGTH_LONG)
+                        .show()
+                }
             }
-            else{
-                Toast.makeText(baseContext,"Error al enviar el email.",Toast.LENGTH_LONG).show()
-            }
+        } else {
+            Toast.makeText(
+                baseContext,
+                "Ingresa un Email Valido",
+                Toast.LENGTH_LONG
+            ).show()
         }
     }
 }
